6c528c982a00a7e6335bf17abb82724f36134a31
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-18 17:48:41 +0800

    main concepts-state and life cycle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+function tick() {
+  ReactDOM.render(<Clock />, document.getElementById('root'));
 }
 
-setInterval(App, 1000);
+setInterval(tick, 1000);
 
-export default App;
+export default tick;
