675b7f01ca4fc8c25d3b5999f32753822d6059c2
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-19 23:43:05 +0800

    specialisation example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import './App.css';
-function Contacts() {
-  return <div className='Contacts' />;
-}
 
-function Chat() {
-  return <div className='Chat' />;
-}
-
-function SplitPane(props) {
+function FancyBorder(props) {
   return (
-    <div className='SplitPane'>
-      <div className='SplitPane-left'>{props.left}</div>
-      <div className='SplitPane-right'>{props.right}</div>
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
     </div>
   );
 }
 
-function App() {
-  return <SplitPane left={<Contacts />} right={<Chat />} />;
+function Dialog(props) {
+  return (
+    <FancyBorder color='blue'>
+      <h1 className='Dialog-title'>{props.title}</h1>
+      <p className='Dialog-message'>{props.message}</p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog
+        title='Mars Exploration Program'
+        message='How should we refer to you?'>
+        <input value={this.state.login} onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>Sign Me Up!</button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
 }
 
 export default App;
